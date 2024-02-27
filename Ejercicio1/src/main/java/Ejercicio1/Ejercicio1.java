d4a5f2942f9c33db1e50c2c6fd5d286fdabd6af5
Sergio <majesergio856@gmail.com>
2024-02-26 20:47:45 -0500

    Repositorio

---
+++ Ejercicio1/src/main/java/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Ejercicio1;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Sergio Majé Montoya
+ */
+
+/*
+En una tienda de computadoras se planea ofrecer a los clientes un descuento que dependerá
+del número de equipos que compre. Si las computadoras son menos de cinco se les dará un
+10% de descuento sobre el total de la compra; si el número de computadoras es mayor o igual
+a cinco pero menos de diez se le otorga un 20% de descuento; y si son 10 o más se les da un
+40% de descuento. El precio de cada computadora es de U$500.
+*/
+
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        /*primero asignamos las variables*/
+        double valorComputadora = 500;
+      Scanner input = new Scanner(System.in)
+              
+      int numeroComputadora;
+      
+      numeroComputadora = input.nextInt();
+    }
+
+   
+            
+             
+                
+            
+        
+        
+        
+        
+    }
+}
