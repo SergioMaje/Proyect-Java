2ad149b6d49bb9f3ba048689e98dcc365d24498b
SergioMaje <160795412+SergioMaje@users.noreply.github.com>
2024-09-18 15:29:39 -0500

    Add files via upload
    nuevos ejercicios

---
+++ POO2/src/EjerciciosArrays/Ejercicio4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjerciciosArrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author majes
+ */
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
---
+++ POO2/src/EjerciciosArrays/Notas.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjerciciosArrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author majes
+ */
+public class Notas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int tamaño = 8;        
+        double[] notas = new double[tamaño];
+        
+        // Así se rellena el arreglo con valores proporcionados por el usuario
+        
+        for (int i = 0; i < tamaño; i++) {
+            System.out.println("Introduce la nota: " + (i + 1) + ":");
+            notas[i] = scanner.nextDouble();
+        }
+        
+        System.out.println("notas introducidas: ");
+        for(double nota: notas){
+            System.out.println(nota);
+        }
+        
+        // le damos una posición a max en el arreglo
+        double max = notas[0];
+        double min = notas[1];
+        
+        //iteramos los datos que se ingresaron en el arreglo
+        
+        for (int i = 0; i < notas.length; i++) {
+            //condicionamos que si alguno de esos datos es mayor a max entonces max se convirtiera en ese dato
+            //así nos aseguramos de que sea el mayor
+            if(notas[i] > max){
+                max = notas[i];
+            }
+        }
+        
+        for (int i = 0; i < notas.length; i++) {
+            if(notas[i] < min){
+                min = notas[i];
+            }
+        }
+        
+        System.out.println("El número máximo es: " + max);
+        System.out.println("El número mínimo es: " + min);
+        
+        /*
+         for (int i = 0; i < 8; i++) {
+            System.out.println("estas son las posiciones " + i);
+            System.out.println("estos son los datos dentro del arreglo:  " + notas[i]);
+        }
+        */
+       
+        
+    }
+}
